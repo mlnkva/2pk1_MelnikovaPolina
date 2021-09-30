e1abaae9330161540cfcaed396d1e97050f0652d
mlnkva <melnikowa243@gmail.com>
2021-09-30 07:26:52 +0300

    test edit

---
+++ pz_5/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace pz_5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("работа с git");
+        }
+    }
+}
