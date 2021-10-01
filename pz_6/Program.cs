0cbc6c83e1100154287b966203512e79f77c34be
mlnkva <melnikowa243@gmail.com>
2021-10-01 08:37:14 +0300

    test edit

---
+++ pz_6/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace pz_6
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double y;
+            double x;
+            x = -2;
+            do
+            {
+                y = (-2.4 * Math.Pow(x, 2) + 5 * x - 3);
+                Console.WriteLine("{0} и {1}", x, y);
+                x += 0.5;
+            }
+            while (x <= 2);
+            Console.WriteLine();
+           
+            
+            
+        }
+    }
+}
