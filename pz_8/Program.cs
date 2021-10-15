d11b8d5543dfc28e0c3b3bbb49b697dc07459cfa
mlnkva <melnikowa243@gmail.com>
2021-10-15 14:58:40 +0300

    test edit

---
+++ pz_8/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace pz_8
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] A = new int[10];
+            Random rnd = new Random();
+            for (int i = 0; i < A.Length; i++)
+            {
+                
+                A[i] = rnd.Next(-10, 11);
+                Console.WriteLine(A[i]);
+                
+            }
+            Console.WriteLine("After reversing:");
+            Array.Reverse(A, 0, 9);
+            foreach (int k in A)
+                Console.WriteLine(k);
+
+
+
+
+
+
+
+        }
+    }
+}
