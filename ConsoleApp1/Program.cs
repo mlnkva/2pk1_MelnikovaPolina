372d44f8161cd0399606bfff5e26d65746fbe47c
mlnkva <melnikowa243@gmail.com>
2021-11-08 22:23:26 +0300

    test.edit

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string testStr = "Тестовая строка 39, нужно извлечь все 8 или мб 2. числа";
+            string numbers = String.Empty;
+
+            for (int i = 0; i < testStr.Length; i++)
+            {
+                if (char.IsDigit(testStr[i]) || char.IsWhiteSpace(testStr[i]))
+                {
+                    numbers += testStr[i];
+                }
+            }
+
+            string[] nums = numbers.Split(" ", StringSplitOptions.RemoveEmptyEntries);
+            foreach (string num in nums)
+            {
+                Console.Write($"{num} ");
+            }
+        }
+    }
+}
