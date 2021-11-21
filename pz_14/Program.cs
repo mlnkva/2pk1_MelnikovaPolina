5ae1d748f87ada6c4e7a63e7edf149689d33aebe
mlnkva <melnikowa243@gmail.com>
2021-11-21 21:06:14 +0300

    test edit

---
+++ pz_14/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.IO;
+using System.Text;
+namespace pz_14
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string f1 = File.ReadAllText(@"C:\Users\admin\Desktop\pz\f1.txt", Encoding.UTF8);
+            string f2 = File.ReadAllText(@"C:\Users\admin\Desktop\pz\f2.txt", Encoding.UTF8);
+            File.WriteAllText (@"C:\Users\admin\Desktop\pz\f.txt", f1, Encoding.UTF8);
+            File.WriteAllText(@"C:\Users\admin\Desktop\pz\f2.txt",
+            File.ReadAllText(@"C:\Users\admin\Desktop\pz\f.txt", Encoding.UTF8),
+            Encoding.UTF8);
+            File.WriteAllText(@"C:\Users\admin\Desktop\pz\f1.txt", f2, Encoding.UTF8);
+
+        }
+
+
+    }
+}
---
+++ pz_tekst(12)/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.IO;
+
+class Program
+{
+    static void Main()
+    {
+        // считываем весь текст из первого и второго файлов
+        string f1 = File.ReadAllText("C:\\File1.txt");
+        string f2 = File.ReadAllText("C:\\File2.txt");
+        // записываем текст из одного в другой файл
+        File.WriteAllText(f1, "C:\\File2.txt");
+        File.WriteAllText(f2, "C:\\File1.txt");
+    }
+}
