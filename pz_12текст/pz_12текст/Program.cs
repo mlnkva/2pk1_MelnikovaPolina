1dc857fc831532ab131133b040e8514d70a7b2d7
mlnkva <91648419+mlnkva@users.noreply.github.com>
2021-11-24 20:44:01 +0500

    Add files via upload

---
+++ pz_12текст/pz_12текст/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Linq;
+
+namespace работа_с_текстом
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            int count = 0;
+            while (true)
+            {
+                Console.WriteLine("Сколько строк вы хотите ввести?");
+                string n = Console.ReadLine();
+                if (int.TryParse(n, out count) && (count > 0))
+                    break;
+                else
+                    Console.WriteLine("Количество строк должно быть целым положительным числом");
+            }
+            string[] stroki = new string[count];
+            for (int i = 0; i < count; i++)
+            {
+                Console.WriteLine($"Введите {i + 1} строку");
+                stroki[i] = Console.ReadLine();
+            }
+            Console.WriteLine("Вы ввели строки:");
+            for (int i = 0; i < count; i++)
+            {
+                Console.WriteLine(stroki[i]);
+            }
+            string union_stroki = stroki[0];
+            for (int i = 1; i < count; i++)
+            {
+                union_stroki = union_stroki + " " + stroki[i];
+            }
+            Console.WriteLine("Объединенная строка:");
+            Console.WriteLine(union_stroki);
+           var result = union_stroki.Split(" ").Distinct().ToArray();
+            string result_string = "";
+            for (int i = 0; i < result.Count(); i++)
+            {
+                result_string = result_string + " " + result[i];
+            }
+            result_string = result_string.Trim();
+            Console.WriteLine("Итоговая строка:");
+            Console.WriteLine(result_string);
+        }
+    }
+}
