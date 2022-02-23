006c97fad1d92553c9f73fddbae3194e75bcb569
mlnkva <91648419+mlnkva@users.noreply.github.com>
2022-02-23 17:33:41 +0300

    Add files via upload

---
+++ ConsoleApp8/ConsoleApp8/Class1.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp
+{
+    internal class ConplexNumber
+    {
+        public double a;
+        public string i;
+        public double b;
+
+        public ConplexNumber(double A, string I, double B)
+        {
+            a = A;
+            i = I;
+            b = B;
+        }
+        public void GetConplexNumber()
+        {
+            Console.WriteLine($"Действительная часть: {a}\nМнимая часть:{i}\nКоэффициент мнимой части:{b}");
+        }
+    }
+}
---
+++ ConsoleApp8/ConsoleApp8/Program.cs
@@ -0,0 +1,25 @@
+﻿using ConsoleApp;
+using System;
+
+namespace Class
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ConplexNumber c = new ConplexNumber(5, "i", 4);
+            c.GetConplexNumber();
+            Console.WriteLine();
+            ConplexNumber c1 = new ConplexNumber(1, "i", 3);
+            c1.GetConplexNumber();
+            Console.WriteLine();
+            ConplexNumber c2 = new ConplexNumber(7, "i", 8);
+            c2.GetConplexNumber();
+            Console.WriteLine();
+            ConplexNumber c3 = new ConplexNumber(2, "i", 9);
+            c3.GetConplexNumber();
+            Console.WriteLine();
+
+        }
+    }
+}
